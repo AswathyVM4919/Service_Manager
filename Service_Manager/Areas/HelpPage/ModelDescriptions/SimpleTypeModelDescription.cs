179418bc3bc1f791dfe865b0c5a316d7f7cd8abc
aswathyvm8292 <aswathyvm8292@gmail.com>
2024-10-18 21:59:32 +0530

    Add project files.

---
+++ Service_Manager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Service_Manager.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Service_Manager/Controllers/ServiceAsstController.cs
@@ -0,0 +1,53 @@
+﻿using Service_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Service_Manager.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ServiceAsstController : ApiController
+    {
+        private MachineTestEntities _dbContext = new MachineTestEntities();
+        public IHttpActionResult GetServiceAsstById(int id)
+        {
+            var service = _dbContext.gn_service_asst.Find(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+            return Ok(service);
+        }
+        public IEnumerable<gn_service_asst> GetAllServiceAsst()
+        {
+            return _dbContext.gn_service_asst.ToList();
+        }
+        public IHttpActionResult SaveServiceAsst(gn_service_asst Service_Asst)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                _dbContext.gn_service_asst.Add(Service_Asst);
+                _dbContext.SaveChanges();
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [HttpOptions]
+        public IHttpActionResult Options()
+        {
+            return Ok();
+        }
+    }
+}
